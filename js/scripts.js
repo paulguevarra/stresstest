954cd2ebe8399d726c4b7cc6ac06c9dd790936be
Joe McKinney and Paul Guevarra <paul@>
2017-07-11 16:53:01 -0700

    modify the css/stylesheet the js/scripts and the stresstest.html as well as start the repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,14 @@
 $(document).ready(function(){
   $("form#transportation_survey").submit(function(event){
     event.preventDefault();
-    $("input:checkbox[name=work-transportation]:checked").each(function(){
-      var workTransportationMode = $(this).val();
-      $('#work-responses').append(workTransportationMode + "<br>");
-    });
-    $("#work-responses").show();
-    $('#transportation_survey').hide();
-    $('#transportation_survey2a').show();
-  });
-//});
-//$(document).ready(function(){
 
-  $("form#transportation_survey2a").submit(function(event){
-    event.preventDefault();
-    $("input:checkbox[name=rec-transportation]:checked").each(function(){
-      var recTransportationMode = $(this).val();
-      $('#rec-responses').append(recTransportationMode + "<br>");
-    });
-    $("#work-responses").hide();
-    $("#rec-responses").show();
-    $('#transportation_survey').hide();
-    $('#transportation_survey2a').hide();
+    var quest1 = ["stomach", "mouth", "fatigue", "headache", "memory"]
+
+    var counter = []
+
+
+    counter += $("input:checkbox[name=health-symptoms]:checked").length.val();
+
+    alert("the counter is " + counter);
   });
 });
